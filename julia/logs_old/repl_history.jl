ebe45084861863ad7ae3c927a56aac725ab15138
Kyle Poe <kyle@kpoe.me>
2021-01-11 11:37:43 -0500

    Moving things around, backed up newer conda environments.

---
+++ julia/logs_old/repl_history.jl
@@ -0,0 +1,15 @@
+# time: 2020-06-14 11:14:33 PDT
+# mode: julia
+	quit()
+# time: 2020-06-14 11:14:38 PDT
+# mode: julia
+	exit
+# time: 2020-06-14 11:14:42 PDT
+# mode: julia
+	exit()
+# time: 2020-06-14 11:15:03 PDT
+# mode: julia
+	quit()
+# time: 2020-06-14 11:15:07 PDT
+# mode: julia
+	exit()
