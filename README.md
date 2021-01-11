1709ca0446b513152921d718fb42cd0aee4c0755
Kyle Poe <kyle@kpoe.me>
2021-01-11 11:22:24 -0500

    Finally got windows dotfiles stuff to work. Needed to change default symlink behavior to not emulate old NTFS default.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dotfiles
-I don't want to have to make these from scratch ever again
+My personal configuration files, I take no responsibility for how bad/poorly maintained/etc they may be.
+
+## Windows Issues
+Some of the JetBrains config files don't play nicely with NTFS, so I worked around this by using a `git sparse-checkout` approach.
+* Clone repo without checkout `git clone _ --no-checkout`
+* Set `git config core.protectNTFS false`
+* Initialize sparse checkout `git sparse-checkout init`
+* Remove whatever the offending files are by appending them to `.git/info/sparse-checkout`, prepended by an `!`
+* Run `git checkout -f HEAD` or `git checkout --`
+[This](https://stackoverflow.com/questions/9572407/git-sparse-checkout-with-exclusion) stack issue helped with this.
--- julia/logs/repl_history.jl
+++ julia/logs/repl_history.jl
@@ -1,15 +1,30 @@
-# time: 2020-06-14 11:14:33 PDT
+# time: 2020-07-11 12:04:31 Pacific Daylight Time
+# mode: shell
+	pwd
+# time: 2020-07-11 12:04:34 Pacific Daylight Time
+# mode: shell
+	dir
+# time: 2020-07-11 12:05:44 Pacific Daylight Time
 # mode: julia
 	quit()
-# time: 2020-06-14 11:14:38 PDT
+# time: 2020-07-11 12:05:47 Pacific Daylight Time
 # mode: julia
-	exit
-# time: 2020-06-14 11:14:42 PDT
+	quit
+# time: 2020-07-11 12:05:49 Pacific Daylight Time
+# mode: julia
+	wxit
+# time: 2020-07-11 12:05:51 Pacific Daylight Time
 # mode: julia
 	exit()
-# time: 2020-06-14 11:15:03 PDT
+# time: 2020-07-11 12:07:46 Pacific Daylight Time
+# mode: julia
+	pwd()
+# time: 2020-07-11 12:07:53 Pacific Daylight Time
+# mode: julia
+	exit()
+# time: 2020-07-11 12:09:45 Pacific Daylight Time
 # mode: julia
 	quit()
-# time: 2020-06-14 11:15:07 PDT
+# time: 2020-07-11 12:09:49 Pacific Daylight Time
 # mode: julia
 	exit()
